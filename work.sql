675002b95bb629ad38bab554717ffb8ef9b4fe53
stece850 <stece850@student.otago.ac.nz>
2020-08-18 16:46:43 +1200

    changed stuff

--- work.sql
+++ work.sql
@@ -1,5 +1,7 @@
 DROP TABLE product; 
 DROP TABLE supplier;  
+DROP TABLE employee; 
+DROP TABLE customer; 
 
 CREATE TABLE supplier (
 	supplierID 				INT 			NOT NULL PRIMARY KEY, 
@@ -24,3 +26,39 @@ CREATE TABLE product(
 		retail_price 			INT 			NOT NULL, 
 		wholesale_price 		INT 			NOT NULL 
 );
+
+CREATE TABLE employee(
+	employeeID				INT 			NOT NULL PRIMARY KEY, 
+	first_name 				varchar2(15)	NOT NULL, 
+	middle_name 			varchar2(15), 
+	lastname				varchar2(15)	NOT NULL, 
+	email					varchar2(30)	NOT NULL, 
+	phone					varchar2(10)	NOT NULL, 
+	position				varchar2(15)	NOT NULL, 
+	branch					varchar2(20)	NOT NULL, 
+	Hire_Date				DATE, 
+	salary					NUMBER(6)		NOT NULL, 
+	street_number			INT(3)			NOT NULL, 
+	street_name 			varchar2(20)	NOT NULL, 
+	city					varchar2(20)	NOT NULL,
+	region					varchar2(15)	NOT NULL,
+	postcode				INT(4)
+); 
+
+CREATE TABLE customer(
+	customerID				INT 			NOT NULL PRIMARY KEY, 
+	first_name 				varchar2(15)	NOT NULL, 
+	middle_name 			varchar2(15), 
+	lastname				varchar2(15)	NOT NULL,
+	username				varchar2(20)	NOT NULL,
+	password				varchar2(30)	NOT NULL, 
+	company					varchar2(20), 
+	phone					varchar2(10)	NOT NULL,
+	street_number			INT(3)			NOT NULL, 
+	street_name 			varchar2(20)	NOT NULL, 
+	city					varchar2(20)	NOT NULL,
+	region					varchar2(15)	NOT NULL,
+	postcode				INT(4), 
+	credit_limit 			NUMBER(6,2), 
+	customer_type 			varchar2(30)	NOT NULL
+); 
