149f549f1c29910ba1a49ba1ee4a94145d8ceef1
stece850 <stece850@student.otago.ac.nz>
2020-08-18 17:06:02 +1200

    carrying over

--- work.sql
+++ work.sql
@@ -76,18 +76,4 @@ CREATE TABLE asgn_invoice(
 	paid 					BOOLEAN			NOT NULL
 );
 
-CREATE TABLE asgn_sale(
-	saleID 					INT 			NOT NULL PRIMARY KEY, 
-	customerID 				INT 			NOT NULL
-		CONSTRAINT customerID_constant REFERENCES customer(customerID), 
 
-	employeeID				INT 			NOT NULL	
-		CONSTRAINT employeeID_constant REFERENCES asgn_employee(employeeID), 
-
-	invoiceID				INT 			NOT NULL
-		CONSTRAINT invoiceID_constant REFERENCES asgn_invoice(invoiceID), 
-
-	sale_date				DATE 			NOT NULL,  
-	totalPrice 				NUMBER(6,2) 	NOT NULL,
-	discount 				NUMBER(2)		NOT NULL 
-); 
