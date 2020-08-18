b23c732e9f78953d8fc5ecae2e0eea5c44eb310e
stece850 <stece850@student.otago.ac.nz>
2020-08-18 16:43:54 +1200

    changed stuff

--- load.sql
+++ load.sql
@@ -1,11 +1,9 @@
-
-
-DROP TABLE product;
-DROP TABLE supplier;  
-DROP TABLE employee; 
-DROP TABLE customer; 
+DROP TABLE product;  
 DROP TABLE sale; 
 DROP TABLE invoice; 
+DROP TABLE supplier; 
+DROP TABLE customer; 
+DROP TABLE employee; 
 
 
 CREATE TABLE supplier (
@@ -20,21 +18,19 @@ CREATE TABLE supplier (
 	preferred_payment		varchar2(15)	NOT NULL 
 ); 
 
-CREATE TABLE product (
-	product_name  			varchar2(20)	NOT NULL, 
-	category				varchar2(20)	NOT NULL, 
-	supplierID				INT		 		NOT NULL
-		CONSTRAINT supplierID_constant REFERENCES supplier(supplierID), 
+CREATE TABLE product(
+        product_name    varchar2(20)    NOT NULL,
+        supplierID	INT             NOT NULL
+                CONSTRAINT supplierID_cnt REFERENCES supplier(supplierID),
+        category        varchar2(20)    NOT NULL, 
+        description 			varchar2(40)	NOT NULL, 
+		brand 					varchar2(20)	NOT NULL, 
+		quantity_in_stock		INT 			NOT NULL, 
+		retail_price 			INT 			NOT NULL, 
+		wholesale_price 		INT 			NOT NULL 
+);
 
-	description 			varchar2(40)	NOT NULL, 
-	brand 					varchar2(20)	NOT NULL, 
-	quantity_in_stock		INT 			NOT NULL, 
-	retail_price 			INT 			NOT NULL, 
-	wholesale_price 		INT 			NOT NULL  
-); 
-
-
-CREATE TABLE employee (
+CREATE TABLE employee(
 	employeeID				INT 			NOT NULL PRIMARY KEY, 
 	first_name 				varchar2(15)	NOT NULL, 
 	middle_name 			varchar2(15), 
@@ -44,8 +40,6 @@ CREATE TABLE employee (
 	position				varchar2(15)	NOT NULL, 
 	branch					varchar2(20)	NOT NULL, 
 	Hire_Date				DATE, 
-	#salary is changed to a number as I have noticed that 
-	#is how he always does it in the lab 
 	salary					NUMBER(6)		NOT NULL, 
 	street_number			INT(3)			NOT NULL, 
 	street_name 			varchar2(20)	NOT NULL, 
@@ -54,7 +48,7 @@ CREATE TABLE employee (
 	postcode				INT(4)
 ); 
 
-CREATE TABLE customer (
+CREATE TABLE customer(
 	customerID				INT 			NOT NULL PRIMARY KEY, 
 	first_name 				varchar2(15)	NOT NULL, 
 	middle_name 			varchar2(15), 
@@ -68,30 +62,11 @@ CREATE TABLE customer (
 	city					varchar2(20)	NOT NULL,
 	region					varchar2(15)	NOT NULL,
 	postcode				INT(4), 
-	# I have also changed this as it seems more logical?? 
 	credit_limit 			NUMBER(6,2), 
 	customer_type 			varchar2(30)	NOT NULL
 ); 
 
-CREATE TABLE sale (
-	saleID 					INT 			NOT NULL PRIMARY KEY, 
-	customerID 				INT 			NOT NULL
-		CONSTRAINT customerID_constant REFERENCES customer(customerID), 
-
-	employeeID				INT 			NOT NULL	
-		CONSTRAINT employeeID_constant REFERENCES employee(employeeID), 
-
-	invoiceID				INT 			NOT NULL
-		CONSTRAINT invoiceID_constant REFERENCES invoice(invoiceID), 
-
-	#had to change name as date cant be a variable name
-	sale_date				DATE 			NOT NULL,  
-	totalPrice 				NUMBER(6,2) 	NOT NULL,#changed to number to work with decimals
-	discount 				NUMBER(2)		NOT NULL 
-); 
-
-
-CREATE TABLE invoice (
+CREATE TABLE invoice(
 	invoiceID 				INT 			NOT NULL PRIMARY KEY,
 	customerID				INT 			NOT NULL
 		CONSTRAINT customerID_constant REFERENCES customer(customerID),
@@ -103,4 +78,20 @@ CREATE TABLE invoice (
 	payment_by_date			DATE 			NOT NULL, 
 	paid 					BOOLEAN			NOT NULL
 );
+
+CREATE TABLE sale(
+	saleID 					INT 			NOT NULL PRIMARY KEY, 
+	customerID 				INT 			NOT NULL
+		CONSTRAINT customerID_constant REFERENCES customer(customerID), 
+
+	employeeID				INT 			NOT NULL	
+		CONSTRAINT employeeID_constant REFERENCES employee(employeeID), 
+
+	invoiceID				INT 			NOT NULL
+		CONSTRAINT invoiceID_constant REFERENCES invoice(invoiceID), 
+
+	sale_date				DATE 			NOT NULL,  
+	totalPrice 				NUMBER(6,2) 	NOT NULL,
+	discount 				NUMBER(2)		NOT NULL 
+); 
 
