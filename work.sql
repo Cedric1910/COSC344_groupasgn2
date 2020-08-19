4470b93336af09b79300aa2c21a145f30aff95cc
stece850 <stece850@student.otago.ac.nz>
2020-08-20 11:12:17 +1200

    customer insert

--- work.sql
+++ work.sql
@@ -71,7 +71,7 @@ CREATE TABLE asgn_invoice(
        invoiceID                        INT                     NOT NULL PRIMARY KEY,
        customerID                       INT                     NOT NULL
                CONSTRAINT supplier_cnst REFERENCES asgn_customer(customerID),
-               saleID				INT 			NOT NULL,
+       saleID									 INT 			NOT NULL,
                date_issued                       DATE                    NOT NULL,
                payment_by_date			DATE 			NOT NULL, 
                paid                              VARCHAR(1)              NOT NULL
